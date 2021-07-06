7c71bfce7f48ba951dd5a2b3667cd7399096033e
Shreyash Detroja <77925372+ShreyashDetroja@users.noreply.github.com>
2021-07-06 12:50:53 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int n = 0;
+    cin >> n;
+    if(n % 2 == 0)
+        cout << "Number is even" << endl;
+    else
+        cout << "Number is odd" << endl;
+
+    return 0;
+}
